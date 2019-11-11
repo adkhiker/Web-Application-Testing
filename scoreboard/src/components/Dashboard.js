242068f6dc0260f705c15c8a28a3df6d27128295
adkhiker <poirier.greg@gmail.com>
2019-11-10 22:53:48 -0500

    Final commit

--- scoreboard/src/components/Dashboard.js
+++ scoreboard/src/components/Dashboard.js
@@ -1,34 +1,45 @@
-import React, {useState} from 'react';
-import Display from './Display';
+import React, { useState } from "react";
+import Display from "./Display";
 const Dashboard = () => {
-    const [balls,setBalls] = useState(0);
-    const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
 
-    const baseballEventHandler =(e)=> {
-        if(e.target.name === 'strike' && strikes < 2) {
-            setStrikes(strikes + 1)
-        } else if(e.target.name === 'ball' && balls < 3) {
-            setBalls(balls + 1)
-        } else if(e.target.name === 'foul' && strikes < 2) {
-            setStrikes(strikes + 1)
-        } else if(e.target.name === 'foul' && strikes === 2) {
-            return;
-        } else {
-            setBalls(0);
-            setStrikes(0);
-        }
+  const baseballEventHandler = e => {
+    if (e.target.name === "strike" && strikes < 2) {
+      setStrikes(strikes + 1);
+    } else if (e.target.name === "ball" && balls < 3) {
+      setBalls(balls + 1);
+    } else if (e.target.name === "foul" && strikes < 2) {
+      setStrikes(strikes + 1);
+    } else if (e.target.name === "foul" && strikes === 2) {
+      return;
+    } else {
+      setBalls(0);
+      setStrikes(0);
     }
+  };
 
-    return (
-        <div>
-            <Display balls={balls} strikes={strikes} />
-            <button name='strike' onClick={baseballEventHandler} data-testid='strikebtn'>Strike</button>
-            <button name='foul' onClick={baseballEventHandler} data-testid='foulbtn'>Foul</button>
-            <button name='hit' onClick={baseballEventHandler} data-testid='hitbtn'>Hit</button>
-            <button name='ball' onClick={baseballEventHandler} data-testid='ballbtn'>Ball</button>
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <Display balls={balls} strikes={strikes} />
+      <button
+        name="strike"
+        onClick={baseballEventHandler}
+        data-testid="strikebtn"
+      >
+        Strike
+      </button>
+      <button name="foul" onClick={baseballEventHandler} data-testid="foulbtn">
+        Foul
+      </button>
+      <button name="hit" onClick={baseballEventHandler} data-testid="hitbtn">
+        Hit
+      </button>
+      <button name="ball" onClick={baseballEventHandler} data-testid="ballbtn">
+        Ball
+      </button>
+    </div>
+  );
+};
 
 export default Dashboard;
--- scoreboard/src/components/Display.js
+++ scoreboard/src/components/Display.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-function Display({strikes, balls}) {
-    return (
-        <div>
-            Strikes
-        <div data-testid='strikes'>{strikes}</div>
-        Balls
-        <div data-testid='balls'>{balls}</div>
-        </div>
-    )
+function Display({ strikes, balls }) {
+  return (
+    <div>
+      Strikes
+      <div data-testid="strikes">{strikes}</div>
+      Balls
+      <div data-testid="balls">{balls}</div>
+    </div>
+  );
 }
 
 export default Display;
